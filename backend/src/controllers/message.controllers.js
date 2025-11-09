45aa7de59d849a7906ad6def025c0cebc6d237e3
Developer-Deepak02 <db23806@gmail.com>
2025-11-09 13:29:54 +0530

    Add validation to sendMessage controller and fix typo in Message model

--- backend/src/controllers/message.controllers.js
+++ backend/src/controllers/message.controllers.js
@@ -39,6 +39,16 @@ export const sendMessage = async (req, res) => {
 		const { id: receiverId } = req.params;
 		const senderId = req.user._id;
 
+    if (!text && !image) {
+      return  res.status(400).json({ message: "Message text or image is required" });
+    }
+    if(senderId.equals(receiverId)){
+      return  res.status(400).json({ message: "Cannot send message to yourself" });
+    }
+    if (!receiverId) {
+      return  res.status(400).json({ message: "Receiver not found" });
+    }
+
 		let imageUrl;
 		if (image) {
 			const uploadResponse = await cloudinary.uploader.upload(image);
--- backend/src/models/Message.js
+++ backend/src/models/Message.js
@@ -14,6 +14,8 @@ const messageSchema = new mongoose.Schema(
     },
     text: {
       type: String,
+      trim: true,
+      maxlenghth: 2000,
     
     },
     image:{
